19aebb05485368d502e29843a70519b77bfa26e7
miaelang98 <161275611+miaelang98@users.noreply.github.com>
2025-02-12 13:01:08 +0900

    Add files via upload

---
+++ data/example_pokemon.py
@@ -0,0 +1,5 @@
+example_pokemon = {
+    "name": "알로라 디그다",
+    "types": ["땅", "강철"],
+    "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/alora_digda.webp"
+}
---
+++ data/pokemons.py
@@ -0,0 +1,32 @@
+pokemons = [
+    {
+        "name": "피카츄",
+        "types": ["전기"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/pikachu.webp"
+    },
+    {
+        "name": "누오",
+        "types": ["물", "땅"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/nuo.webp",
+    },
+    {
+        "name": "갸라도스",
+        "types": ["물", "비행"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/garados.webp",
+    },
+    {
+        "name": "개굴닌자",
+        "types": ["물", "악"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/frogninja.webp"
+    },
+    {
+        "name": "루카리오",
+        "types": ["격투", "강철"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/lukario.webp"
+    },
+    {
+        "name": "에이스번",
+        "types": ["불꽃"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/acebun.webp"
+    },
+]
